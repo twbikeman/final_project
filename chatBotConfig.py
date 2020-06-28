09ab2bbc42c4b0ef957b6cca8ea05fc64e1a02ed
Bryan Tsai <twbikeman@gmail.com>
2020-06-28 08:21:24 +0800

    flask finshed

---
+++ chatBotConfig.py
@@ -0,0 +1,5 @@
+channel = {
+'channelId': '1653832096',
+'channelSecret': '2a08c74bf04c2b30b336386a7d60a35c',
+'accessToken': 'Qs7IgBANeZjkaWrtdQu+rbdha+XqR43IyyHV2qwCetpdugAfp9+nJL126llPThWPDqe1qHXo0nyqj2NsRhWANZ/BU3BCFJi8/D4fpwuxGSHeu3eboIjgpwBLZ91JOKneGnci5L3WT41znIsioZmTTgdB04t89/1O/w1cDnyilFU='
+}
---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, Response
+
+from linebot import WebhookParser, LineBotApi
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+
+
+import chatBotConfig
+
+#from fetch import fetch
+
+
+app = Flask(__name__) 
+
+line_bot_api = LineBotApi(chatBotConfig.channel['accessToken'])
+
+#query = fetch()
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']  
+    body = request.get_data(as_text = True)
+    events = WebhookParser(chatBotConfig.channel['channelSecret']).parse(body, signature)
+    lineId = events[0].source.user_id
+#    bus = events[0].message.text
+#    query.set(bus)
+#    pic_url = 'https://storage.googleapis.com/aibigdata-ntut-107598064.appspot.com/bus/1593174590.jpg'
+#    if (query.gettime()):
+#        text = query.getbus() + '\n' + query.getdest() + '\n' + query.gettime()
+#    else:
+#        text = query.getbus() + '\n' + query.getdest() + '\n' + query.getstatus()
+
+
+    text = events[0].message.text
+    print('-----------------------\n' + text)
+    line_bot_api.push_message(lineId, TextSendMessage(text = text, disable_web_page_preview = True))
+#    line_bot_api.push_message(lineId, ImageSendMessage(original_content_url='https://storage.googleapis.com/aibigdata-ntut-107598064.appspot.com/bus/1593174590.jpg', preview_image_url = 'https://storage.googleapis.com/aibigdata-ntut-107598064.appspot.com/bus/1593174590.jpg'))
+    return 'OK'
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
